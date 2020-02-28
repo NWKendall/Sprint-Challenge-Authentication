e623751e54a60cbe7584a786a6af8f1dfec3c29e
nicbongo <nicbongo@protonmail.com>
2020-02-28 13:22:34 -0400

    register test

---
+++ jokes/jokesrouter.spec.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const request = require('supertest');
+
+const router = require('./jokes-router.js');
+
+
+// jokes
+describe('get /jokes ', () => {
+  it('returns a status 200 OK', () => {
+    request(router)
+      .get('/')
+      .then(res => {
+        expect(res.status).toBe(200);    
+        })
+    })
+})
+
